0ce512b20a7f419934f1e1e54b1b9aca5273c43b
jeffIshmael <jeffishmael141@gmail.com>
2025-07-03 11:54:26 +0300

    debug: vercel deployment error

--- packages/react-app/app/task/[id]/page.tsx
+++ packages/react-app/app/task/[id]/page.tsx
@@ -1,6 +1,5 @@
-// REMOVE 'use client'; ✅ this should be a server component
 
-import TaskPage from './TaskPage';
+import TaskPage from '../TaskPage';
 
 export default function Page({ params }: { params: { id: string } }) {
   return <TaskPage taskId={params.id} />;
